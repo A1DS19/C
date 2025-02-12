eacf3765031cfb2a183acb6c1c2296a58b2576e5
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2025-02-12 12:44:08 -0800

    strings

---
+++ strings/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+  char str[] = "qwe123";
+  char str2[] = {'1', '2', '3', '4', '5'};
+
+  printf("%s", str);
+
+  return EXIT_SUCCESS;
+}
